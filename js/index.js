fa9259c0fd15ee77a79ff9af60c9a8919c0605d3
Good <dolinik.ig@mail.ru>
2020-07-15 23:06:37 +0300

    Full version 1.0.1

--- js/index.js
+++
@@ -1,8 +0,0 @@
-import '../scss/style.scss';
-import initSwiperFunction from './swiper';
-import modalFunctions from './button_swiper';
-import buttonFunctionsRead from './modal';
-
-initSwiperFunction();
-modalFunctions();
-buttonFunctionsRead();
